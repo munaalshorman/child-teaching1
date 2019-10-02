7f215b6738ff52f77a0a9bf2d4b754f39ac0e4db
muna alshorman <munaalshorman123@gmail.com>
2019-10-02 23:02:51 +0300

    aa

---
+++ js/js.js
@@ -0,0 +1,24 @@
+var name=prompt("WHAT IS YOUR NAME?")
+alert("hello  "+name);
+function myFunction() {
+    confirm("Press a button!");
+  }
+ myFunction()
+var age =prompt("Enter your age") ;
+var result ;
+if (age < 10)   {
+    result = 'read from under 10 age';
+}else if( age >=10 && age <= 15){
+    result = 'read from above 10';
+}else if (age>16) {
+    result='read from reading section';  }
+document .write( ' <h3>' +result + '</h3>');
+console.log('hi');
+var calculate=prompt ("5!*4+3");
+
+while (calculate!= 643) {
+    calculate = prompt ("Sorry incorrect awnswer");
+ }
+if (calculate == 643) {
+alert ("this is true");
+} 
